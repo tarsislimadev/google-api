61971ee56d2cc8270df4b6ee948fc8f1dd3c3da2
Tarsis <br.tmvdl@gmail.com>
2023-09-03 17:34:07 -0300

    Adding "CURL token" file

---
+++ curl.token.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+
+# inputs
+
+. .env
+
+curl -sL --data "grant_type=refresh_token" \
+  --data "client_id=${CLIENT_ID}" \
+  --data "client_secret=${CLIENT_SECRET}" \
+  --data "refresh_token=${REFRESH_TOKEN}" \
+  "https://www.googleapis.com/oauth2/v3/token"
